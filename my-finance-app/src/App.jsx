db30724d7987193d9a1e337995ab7fb06fab81de
regi1297 <reginald_miranda@hotmail.com>
2024-07-07 18:36:13 -0300

    Atualização do JWT

--- my-finance-app/src/App.jsx
+++ my-finance-app/src/App.jsx
@@ -8,7 +8,7 @@ import Orcamento from './pages/Orcamento';
 import ControleSemanal from './pages/ControleSemanal';
 import Metas from './pages/Metas';
 import Agenda from './pages/Agenda';
-import Receitas from './pages/Receitas'; // Importe a página de Receitas
+import Receitas from './pages/Receitas';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -24,23 +24,24 @@ const App = () => {
       <nav>
         <ul>
           <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/receitas">Receitas</Link></li>
           <li><Link to="/dividas">Despesas</Link></li>
+          <li><Link to="/metas">Metas</Link></li>
           <li><Link to="/orcamento">Orçamento</Link></li>
           <li><Link to="/controle-semanal">Controle Semanal</Link></li>
-          <li><Link to="/metas">Metas</Link></li>
           <li><Link to="/agenda">Agenda</Link></li>
-          <li><Link to="/receitas">Receitas</Link></li> {/* Adicione o link para Receitas */}
         </ul>
       </nav>
       <Container>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/receitas" element={<Receitas />} />
           <Route path="/dividas" element={<Dividas />} />
+          <Route path="/metas" element={<Metas />} />
           <Route path="/orcamento" element={<Orcamento />} />
           <Route path="/controle-semanal" element={<ControleSemanal />} />
-          <Route path="/metas" element={<Metas />} />
           <Route path="/agenda" element={<Agenda />} />
-          <Route path="/receitas" element={<Receitas />} /> {/* Rota para a página de Receitas */}
+
         </Routes>
       </Container>
     </Router>
